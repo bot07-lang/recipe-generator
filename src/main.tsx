87b512846e2ec40261a95ec1fbacfe8d4222ab25
Akash Dangwal <akashdangwal@Nainas-MacbookAir.local>
2025-10-27 14:03:35 +0530

    Fix TypeScript build errors for Vercel deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './routes/App.tsx';
-import Home from './routes/Home.tsx';
-import Generator from './routes/Generator.tsx';
-import Developer from './routes/Developer.tsx';
+import App from './routes/App';
+import Home from './routes/Home';
+import Generator from './routes/Generator';
+import Developer from './routes/Developer';
 import './styles.css';
 
 const router = createBrowserRouter([
